3221eb9cbdcc95c7f91fb4a70fd99763eda369f8
Marco Cruz <marco24cruz08@gmail.com>
2024-12-23 13:05:16 -0600

    ✨ Add Set.prototype.difference method for compatibility with older Node.js versions

--- 2024/23-encuentra-los-numeros-perdidos/index.js
+++ 2024/23-encuentra-los-numeros-perdidos/index.js
@@ -1,3 +1,13 @@
+/* eslint-disable func-names */
+/* eslint-disable no-extend-native */
+Set.prototype.difference = function (set) {
+  return new Set([...this].filter((x) => !set.has(x)));
+};
+
+// La función de set.difference() esta disponible desde la version 22 de nodejs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/difference#browser_compatibility
+// Para poder utilizarla en versiones anteriores de nodejs, se debe de agregar la función al prototipo de Set
+
 function findMissingNumbers(nums) {
   const maxNumber = Math.max(...nums);
   const fullSetOfNumbers = new Set(
